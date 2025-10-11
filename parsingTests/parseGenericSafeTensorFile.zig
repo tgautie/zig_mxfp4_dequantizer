98efe69b5bcd110a94735e29c252aa8910da7b02
tgautie <tgautie@joko.com>
2025-10-11 19:36:47 +0200

    Small refacto

--- parsingTests/parseGenericSafeTensorFile.zig
+++ parsingTests/parseGenericSafeTensorFile.zig
@@ -9,42 +9,51 @@ const TensorConfig = struct {
 };
 
 pub fn main() !void {
-    const header_size = std.mem.readInt(u64, input[0..8], .little);
+    const allocator = std.heap.page_allocator;
+
+    const file_buffer = input;
+
+    const header_size = read_header_size(file_buffer);
     std.debug.print("Header size: {}\n", .{header_size});
 
-    const header_buf = input[8 .. 8 + header_size];
-    std.debug.print("Header content: {s}\n", .{header_buf});
-
-    const allocator = std.heap.page_allocator;
-    const parsed_json = try std.json.parseFromSlice(std.json.Value, allocator, header_buf, .{});
-    defer parsed_json.deinit();
-
-    try iterateOnTensors(parsed_json.value, header_size, allocator);
+    try read_header_config(file_buffer, header_size, allocator);
 }
 
-fn iterateOnTensors(
-    root: std.json.Value,
+fn read_header_size(file_buffer: []const u8) u64 {
+    return std.mem.readInt(u64, file_buffer[0..8], .little);
+}
+
+fn read_header_config(
+    file_buffer: []const u8,
     header_size: u64,
     allocator: std.mem.Allocator,
 ) !void {
-    if (root == .object) {
-        var it = root.object.iterator();
-        while (it.next()) |entry| {
-            const tensor_name = entry.key_ptr.*;
-            if (std.mem.eql(u8, tensor_name, "__metadata__")) {
-                continue;
-            }
+    const header_buf = file_buffer[8 .. 8 + header_size];
+    std.debug.print("Header content: {s}\n", .{header_buf});
 
-            const value = entry.value_ptr.*;
-            const tensor_config = try parseTensorConfig(tensor_name, value, allocator);
-            std.debug.print("Tensor {s}: {s}, shape: {any}, data offsets: {any}\n", .{ tensor_config.tensor_name, tensor_config.dtype, tensor_config.shape, tensor_config.data_offsets });
+    const parsed_json = try std.json.parseFromSlice(std.json.Value, allocator, header_buf, .{});
+    defer parsed_json.deinit();
+    const root = parsed_json.value;
 
-            const tensor_values_buf = input[8 + header_size + tensor_config.data_offsets[0] .. 8 + header_size + tensor_config.data_offsets[1]];
-            const tensor_values = std.mem.bytesAsSlice(f32, tensor_values_buf);
-            std.debug.print("Tensor {s} values: {any}\n", .{ tensor_name, tensor_values });
-        }
-    } else {
+    if (root != .object) {
         std.debug.print("Root is not an object\n", .{});
+        return;
+    }
+
+    var it = root.object.iterator();
+    while (it.next()) |entry| {
+        const tensor_name = entry.key_ptr.*;
+        if (std.mem.eql(u8, tensor_name, "__metadata__")) {
+            continue;
+        }
+
+        const value = entry.value_ptr.*;
+        const tensor_config = try parseTensorConfig(tensor_name, value, allocator);
+        std.debug.print("Tensor {s}: {s}, shape: {any}, data offsets: {any}\n", .{ tensor_config.tensor_name, tensor_config.dtype, tensor_config.shape, tensor_config.data_offsets });
+
+        const tensor_values_buf = input[8 + header_size + tensor_config.data_offsets[0] .. 8 + header_size + tensor_config.data_offsets[1]];
+        const tensor_values = std.mem.bytesAsSlice(f32, tensor_values_buf);
+        std.debug.print("Tensor {s} values: {any}\n", .{ tensor_name, tensor_values });
     }
 }
 
