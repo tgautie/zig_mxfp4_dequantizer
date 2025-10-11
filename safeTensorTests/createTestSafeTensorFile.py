be914078eb69edeefc20f86cbabe15d107e69dba
tgautie <tgautie@joko.com>
2025-10-11 16:52:22 +0200

    Small test setup for F32 safetensor file

---
+++ safeTensorTests/createTestSafeTensorFile.py
@@ -0,0 +1,8 @@
+import torch
+from safetensors.torch import save_file
+
+tensors = {
+    "embedding": torch.zeros((2, 2)),
+    "attention": torch.zeros((2, 3))
+}
+save_file(tensors, "test.safetensors")
