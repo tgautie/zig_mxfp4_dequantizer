fd071b7cc763ed409abf4185387b6e67b11eac3d
tgautie <tgautie@joko.com>
2025-10-23 09:25:49 +0200

    Parse MXFP4 configs

--- explorationTests/safeTensorTests/createTestSafeTensorFile.py
+++ explorationTests/safeTensorTests/createTestSafeTensorFile.py
@@ -1,9 +1,12 @@
 import torch
-from safetensors.torch import save_file
+from safetensors.torch import save_file;
+
+N = 10
 
 tensors = {
-    "embedding": torch.ones((2, 2)),
-    "attention": torch.ones((2, 3)),
-    "zeros": torch.zeros((2, 3, 4, 5))
+    "tensor_with_ones_scales": torch.ones((N), dtype=torch.uint8),
+    "tensor_with_ones_blocks": torch.ones((N, 16), dtype=torch.uint8),
+    "tensor_with_zeros_scales": torch.zeros((N), dtype=torch.uint8),
+    "tensor_with_zeros_blocks": torch.zeros((N, 16), dtype=torch.uint8),
 }
-save_file(tensors, "mixed.safetensors")
+save_file(tensors, "test_mxfp4.safetensors")
