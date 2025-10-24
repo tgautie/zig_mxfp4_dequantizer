7d2a69b5c484dae7cf6dbe62a37f97501f371abe
tgautie <tgautie@joko.com>
2025-10-24 19:48:25 +0200

    Add unit tests

--- mxfp4_config_parser.zig
+++ mxfp4_config_parser.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
-const TensorConfig = @import("safetensors_file_parser.zig").TensorConfig;
+const TensorConfig = @import("safetensors_header_parser.zig").TensorConfig;
 
 pub const Mxfp4TensorConfig = struct {
     mxfp4_tensor_name: []const u8,
@@ -16,11 +16,11 @@ pub fn get_mxfp4_tensor_configs(
     allocator: std.mem.Allocator,
     tensor_configs: std.ArrayList(TensorConfig),
 ) !std.ArrayList(Mxfp4TensorConfig) {
-    var mxfp4_tensor_configs = try std.ArrayList(Mxfp4TensorConfig).initCapacity(allocator, 1000000);
-    errdefer mxfp4_tensor_configs.deinit(allocator);
-
     const scales_map = try get_scales_config_map(allocator, tensor_configs);
     defer scales_map.deinit();
+
+    var mxfp4_tensor_configs = try std.ArrayList(Mxfp4TensorConfig).initCapacity(allocator, 1000000);
+    errdefer mxfp4_tensor_configs.deinit(allocator);
 
     for (tensor_configs.items) |tensor_config| {
         if (!is_mxfp4_blocks_tensor_name(tensor_config.tensor_name)) continue;
@@ -52,8 +52,18 @@ fn is_mxfp4_scales_tensor_name(tensor_name: []const u8) bool {
     return std.mem.endsWith(u8, tensor_name, "_scales");
 }
 
+test is_mxfp4_scales_tensor_name {
+    try std.testing.expectEqual(true, is_mxfp4_scales_tensor_name("test_scales"));
+    try std.testing.expectEqual(false, is_mxfp4_scales_tensor_name("test_blocks"));
+}
+
 fn is_mxfp4_blocks_tensor_name(tensor_name: []const u8) bool {
     return std.mem.endsWith(u8, tensor_name, "_blocks");
+}
+
+test is_mxfp4_blocks_tensor_name {
+    try std.testing.expectEqual(true, is_mxfp4_blocks_tensor_name("test_blocks"));
+    try std.testing.expectEqual(false, is_mxfp4_blocks_tensor_name("test_scales"));
 }
 
 fn get_mxfp4_tensor_name(tensor_name: []const u8) []const u8 {
@@ -64,6 +74,12 @@ fn get_mxfp4_tensor_name(tensor_name: []const u8) []const u8 {
     } else {
         return tensor_name;
     }
+}
+
+test get_mxfp4_tensor_name {
+    try std.testing.expectEqualStrings("test", get_mxfp4_tensor_name("test_scales"));
+    try std.testing.expectEqualStrings("test", get_mxfp4_tensor_name("test_blocks"));
+    try std.testing.expectEqualStrings("test", get_mxfp4_tensor_name("test"));
 }
 
 fn format_mxfp4_tensor_config(mxfp4_tensor_name: []const u8, scales_tensor_config: TensorConfig, blocks_tensor_config: TensorConfig) Mxfp4TensorConfig {
@@ -85,4 +101,10 @@ fn get_total_tensor_values(shape: []const u32) u32 {
         total_values *= dimension_size;
     }
     return total_values;
+}
+
+test get_total_tensor_values {
+    try std.testing.expectEqual(5, get_total_tensor_values(&[_]u32{5}));
+    try std.testing.expectEqual(6, get_total_tensor_values(&[_]u32{ 2, 3 }));
+    try std.testing.expectEqual(24, get_total_tensor_values(&[_]u32{ 2, 3, 4 }));
 }
