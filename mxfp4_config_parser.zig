1bb880f8c039fcdd58bc3438835e4fcec18803b2
tgautie <tgautie@joko.com>
2025-10-24 19:31:15 +0200

    Start implementing full system

---
+++ mxfp4_config_parser.zig
@@ -0,0 +1,88 @@
+const std = @import("std");
+const TensorConfig = @import("safetensors_file_parser.zig").TensorConfig;
+
+pub const Mxfp4TensorConfig = struct {
+    mxfp4_tensor_name: []const u8,
+    blocks_count: u32,
+    scales_dtype: []const u8,
+    blocks_dtype: []const u8,
+    scales_shape: []const u32,
+    blocks_shape: []const u32,
+    scales_absolute_offsets: [2]u32,
+    blocks_absolute_offsets: [2]u32,
+};
+
+pub fn get_mxfp4_tensor_configs(
+    allocator: std.mem.Allocator,
+    tensor_configs: std.ArrayList(TensorConfig),
+) !std.ArrayList(Mxfp4TensorConfig) {
+    var mxfp4_tensor_configs = try std.ArrayList(Mxfp4TensorConfig).initCapacity(allocator, 1000000);
+    errdefer mxfp4_tensor_configs.deinit(allocator);
+
+    const scales_map = try get_scales_config_map(allocator, tensor_configs);
+    defer scales_map.deinit();
+
+    for (tensor_configs.items) |tensor_config| {
+        if (!is_mxfp4_blocks_tensor_name(tensor_config.tensor_name)) continue;
+
+        const mxfp4_tensor_name = get_mxfp4_tensor_name(tensor_config.tensor_name);
+        if (scales_map.get(mxfp4_tensor_name)) |scales_tensor_config| {
+            const mxfp4_tensor_config = format_mxfp4_tensor_config(mxfp4_tensor_name, scales_tensor_config, tensor_config);
+            try mxfp4_tensor_configs.append(allocator, mxfp4_tensor_config);
+        } else {
+            std.debug.print("Corresponding scales tensor config not found for blocks tensor {s}\n", .{mxfp4_tensor_name});
+            continue;
+        }
+    }
+
+    return mxfp4_tensor_configs;
+}
+
+fn get_scales_config_map(allocator: std.mem.Allocator, tensor_configs: std.ArrayList(TensorConfig)) !std.StringHashMap(TensorConfig) {
+    var scales_map = std.StringHashMap(TensorConfig).init(
+        allocator,
+    );
+
+    for (tensor_configs.items) |tensor_config| {
+        if (is_mxfp4_scales_tensor_name(tensor_config.tensor_name)) try scales_map.put(get_mxfp4_tensor_name(tensor_config.tensor_name), tensor_config);
+    }
+}
+
+fn is_mxfp4_scales_tensor_name(tensor_name: []const u8) bool {
+    return std.mem.endsWith(u8, tensor_name, "_scales");
+}
+
+fn is_mxfp4_blocks_tensor_name(tensor_name: []const u8) bool {
+    return std.mem.endsWith(u8, tensor_name, "_blocks");
+}
+
+fn get_mxfp4_tensor_name(tensor_name: []const u8) []const u8 {
+    if (is_mxfp4_scales_tensor_name(tensor_name)) {
+        return tensor_name[0 .. tensor_name.len - "_scales".len];
+    } else if (is_mxfp4_blocks_tensor_name(tensor_name)) {
+        return tensor_name[0 .. tensor_name.len - "_blocks".len];
+    } else {
+        return tensor_name;
+    }
+}
+
+fn format_mxfp4_tensor_config(mxfp4_tensor_name: []const u8, scales_tensor_config: TensorConfig, blocks_tensor_config: TensorConfig) Mxfp4TensorConfig {
+    return Mxfp4TensorConfig{
+        .mxfp4_tensor_name = mxfp4_tensor_name,
+        .blocks_count = get_total_tensor_values(scales_tensor_config.shape),
+        .scales_dtype = scales_tensor_config.dtype,
+        .blocks_dtype = blocks_tensor_config.dtype,
+        .scales_shape = scales_tensor_config.shape,
+        .blocks_shape = blocks_tensor_config.shape,
+        .scales_absolute_offsets = scales_tensor_config.data_absolute_offsets,
+        .blocks_absolute_offsets = blocks_tensor_config.data_absolute_offsets,
+    };
+}
+
+fn get_total_tensor_values(shape: []const u32) u32 {
+    var total_values: u32 = 1;
+    for (shape) |dimension_size| {
+        total_values *= dimension_size;
+    }
+    return total_values;
+}
---
+++ safetensors_header_parser.zig
@@ -0,0 +1,77 @@
+const std = @import("std");
+
+pub const TensorConfig = struct {
+    tensor_name: []const u8,
+    dtype: []const u8,
+    shape: []const u32,
+    data_absolute_offsets: [2]u32,
+};
+
+pub fn parse_safetensors_header(
+    file_path: []const u8,
+    allocator: std.mem.Allocator,
+) !std.ArrayList(TensorConfig) {
+    var file: std.fs.File = try std.fs.cwd().openFile(file_path, .{ .mode = .read_only });
+    var buffer: [1024]u8 = undefined;
+    var file_reader = file.reader(&buffer);
+
+    const header_size = std.mem.readInt(u64, try file_reader.interface.takeArray(8), .little);
+
+    var tensor_configs = try std.ArrayList(TensorConfig).initCapacity(allocator, 1000000);
+    errdefer tensor_configs.deinit(allocator);
+
+    const header_buf = try file_reader.interface.readAlloc(allocator, header_size);
+    defer allocator.free(header_buf);
+
+    const parsed_json = try std.json.parseFromSlice(std.json.Value, allocator, header_buf, .{});
+    defer parsed_json.deinit();
+    const root = parsed_json.value;
+
+    if (root != .object) {
+        return error.RootIsNotAnObject;
+    }
+
+    var it = root.object.iterator();
+    while (it.next()) |entry| {
+        const tensor_name = entry.key_ptr.*;
+        if (std.mem.eql(u8, tensor_name, "__metadata__")) {
+            continue;
+        }
+
+        const value = entry.value_ptr.*;
+        const tensor_config = try parse_tensor_config(allocator, tensor_name, value, header_size);
+        try tensor_configs.append(allocator, tensor_config);
+    }
+
+    return tensor_configs;
+}
+
+fn parse_tensor_config(
+    allocator: std.mem.Allocator,
+    tensor_name: []const u8,
+    json_config: std.json.Value,
+    header_size: u64,
+) !TensorConfig {
+    const parsed_tensor_config = try std.json.parseFromValue(ParsedTensorConfig, allocator, json_config, .{});
+    defer parsed_tensor_config.deinit();
+
+    const name_copy = try allocator.dupe(u8, tensor_name);
+    const dtype_copy = try allocator.dupe(u8, parsed_tensor_config.value.dtype);
+    const shape_copy = try allocator.dupe(u32, parsed_tensor_config.value.shape);
+    const data_offsets = parsed_tensor_config.value.data_offsets;
+    const total_header_size = 8 + @as(u32, @intCast(header_size));
+    const absolute_data_offsets = [2]u32{ total_header_size + data_offsets[0], total_header_size + data_offsets[1] };
+
+    return TensorConfig{
+        .tensor_name = name_copy,
+        .dtype = dtype_copy,
+        .shape = shape_copy,
+        .data_absolute_offsets = absolute_data_offsets,
+    };
+}
+
+const ParsedTensorConfig = struct {
+    dtype: []const u8,
+    shape: []const u32,
+    data_offsets: [2]u32,
+};
