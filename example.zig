b115a722724acdac9fffa79c7be9889f8eee919d
tgautie <tgautie@joko.com>
2025-10-26 10:24:52 +0100

    Add example

---
+++ example.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+const tensorReaders = @import("tensorReaders.zig");
+
+// Insert your safetensors file path here 🙌
+const file_path = "testSafetensors/simple_test.safetensors";
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+
+    var dequantizedSafetensors = try tensorReaders.DequantizedMxfp4TensorReaders.init(allocator, file_path);
+    defer dequantizedSafetensors.deinit(allocator);
+
+    std.debug.print("Dequantizing the first 100 bytes on the fly from the following MXFP4 tensors:\n", .{});
+
+    for (dequantizedSafetensors.readers.items) |reader| {
+        std.debug.print("{s}\n", .{reader.name});
+        const buffer = try reader.interface.takeArray(100);
+        for (buffer) |b| {
+            std.debug.print("{x} ", .{b});
+        }
+        std.debug.print("\n", .{});
+    }
+}
